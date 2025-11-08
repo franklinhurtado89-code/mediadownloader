464edf9b62dd61b7c97198802b9f9963040d9d54
frankhs899 <franklinhurtadosalazar@gmail.com>
2025-11-08 17:21:31 -0600

    feat(info): Create get_info app for data extraction
    The info_view functionality has been moved from the core app to a new get_info app to improve modularity and code organization

--- config/settings.py
+++ config/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
 
     'core',
+    'get_info',
 ]
 
 MIDDLEWARE = [
--- core/urls.py
+++ core/urls.py
@@ -1,8 +1,7 @@
 from django.urls import path
 
-from .views import home_view, info_view
+from .views import home_view
 
 urlpatterns = [
     path('', home_view),
-    path('info/', info_view, name='info'),
 ]
